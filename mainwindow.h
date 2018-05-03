37c031a0a4a6083c3c8310cbbe37ac7c5282a33c
JJXiangJiaoJun <591495195@qq.com>
2018-05-03 20:25:53 +0800

    添加动态曲线绘图功能

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "qchartviewer.h"
+#include "chartdir.h"
+#include <QDialog>
+#include <QDateTime>
+#include <QTimer>
+#include <QLabel>
+#include <QScrollbar>
+#include "qdoublebufferedqueue.h"
+#include "randomwalk.h"
+#include "performancetimer.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -15,6 +26,67 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+private:
+    Ui::MainWindow *ui;
+
+    //Qcharviewer绘图控件
+    QFrame       *leftFrame;
+    QChartViewer *m_ChartViewer;        // QChartViewer control
+    //******************************************************************************
+    // The random data source
+    RandomWalk *dataSource;
+
+    // Handles callback from the RandomWalk
+    static void OnData(void *self, double elapsedTime, double series0, double series1);
+
+    // A thread-safe queue with minimal read/write contention
+    struct DataPacket
+    {
+        double elapsedTime;
+        double series0;
+        double series1;
+    };
+    QDoubleBufferedQueue<DataPacket> buffer;//每组数据有三个
+
+    // The number of samples per data series used in this demo
+    //显示缓冲区的图像大小
+    static const int sampleSize = 10000;
+
+    // The full range is initialized to 180 seconds. It will extend when more data are available.
+    static const int initialFullRange = 180;
+
+    // The visible range is initialized to 30 seconds.
+    //视野中可视化的秒数，非常重要的一个量
+    static const int initialVisibleRange = 1;
+
+    // The maximum zoom in is 5 seconds.
+    static const int zoomInLimit = 1;
+
+    long msCount;
+    long msStartCount;
+    PerformanceTimer *timer;
+    //******************************************************************************
+
+    // If the track cursor is at the end of the data series, we will automatic move the track
+    // line when new data arrives.
+    double trackLineEndPos;
+    bool trackLineIsAtEnd;
+
+    double m_timeStamps[sampleSize];        // The timestamps for the data series
+    double m_dataSeriesA[sampleSize];       // The values for the data series A
+    double m_dataSeriesB[sampleSize];       // The values for the data series B
+
+    int m_currentIndex;                     // Index of the array position to which new values are added.
+
+    //QViewPortControl *m_ViewPortControl;    // QViewPortControl
+    QTimer *m_ChartUpdateTimer;             // The chart update timer
+
+    void drawChart(QChartViewer *viewer);           // Draw chart
+    void drawFullChart(QViewPortControl *vpc);      // Draw full chart
+    double trackLineLabel(XYChart *c, int mouseX);  // Draw track cursor
+
+
 private slots:
     void on_Start_btn_clicked();
 
@@ -22,8 +94,12 @@ private slots:
 
     void on_Setting_control_para_triggered();
 
-private:
-    Ui::MainWindow *ui;
+    void onMouseUsageChanged(int mouseUsage);       // Pointer/zoom in/zoom out button clicked
+    void onSave(bool);                              // Save button clicked
+    void onMouseMovePlotArea(QMouseEvent *event);   // Mouse move on plot area
+    void onChartUpdateTimer();                      // Update the chart.
+    void onViewPortChanged();                       // Viewport has changed
+    void slotFuction();                             //100ms多媒体定时器
 };
 
 #endif // MAINWINDOW_H
--- pop_wil.pro
+++ pop_wil.pro
@@ -11,11 +11,23 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = pop_wil
 TEMPLATE = app
 
+INCLUDEPATH += ../pop_wil/charview_include
+
+#注： 1、-L 参数指定 .lib/.a 文件的位置
+#        2、-l  参数指定导入库文件名(不要加扩展名)
+#        3、另外，导入库文件的路径中，反斜杠用的是向右倾斜的
+#        4、Qt要隐式调用DLL的话，须把dll文件放在程序的输出目录中，lib所存放的文件路径随便，但是须由上述.pro文件中的LIBS指定语句指定其所在文件夹，.h文件放在程序当前目录下即可。
+
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked as deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
-DEFINES += QT_DEPRECATED_WARNINGS
+
+
+DEFINES += QT_DEPRECATED_WARNINGS CHARTDIR_HIDE_OBSOLETE _CRT  _SECURE_NO_WARNINGS
+
+
+
 
 # You can also make your code fail to compile if you use deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -28,13 +40,24 @@ SOURCES += \
         mainwindow.cpp \
     loginscreen.cpp \
     channel_param.cpp \
-    control_param.cpp
+    control_param.cpp \
+    performancetimer.cpp \
+    qchartviewer.cpp \
+    randomwalk.cpp \
+
+
 
 HEADERS += \
-        mainwindow.h \
+    mainwindow.h \
     loginscreen.h \
-    channel_param.h \
-    control_param.h
+    control_param.h \
+    qdoublebufferedqueue.h \
+    performancetimer.h \
+    qchartviewer.h \
+    randomwalk.h \
+    channel_param.h
+
+
 
 FORMS += \
         mainwindow.ui \
@@ -44,3 +67,20 @@ FORMS += \
 
 RESOURCES += \
     assets.qrc
+
+
+
+#qcharview动态链接库
+
+LIBS += -lwinmm  #PerformanceTimer required
+win32:LIBS += ../pop_wil/lib/chartdir60.lib
+win32:QMAKE_POST_LINK = copy /Y ..\\pop_wil\\lib\\chartdir60.dll $(DESTDIR)
+
+macx:LIBS += -L./lib -lchartdir
+macx:QMAKE_POST_LINK += mkdir -p \"`dirname $(TARGET)`/../Frameworks\";
+macx:QMAKE_POST_LINK += cp ../pop_wil/lib/libchartdir.6.dylib \"`dirname $(TARGET)`/../Frameworks\";
+macx:QMAKE_POST_LINK += install_name_tool -change libchartdir.6.dylib \
+     \@loader_path/../Frameworks/libchartdir.6.dylib \"$(TARGET)\";
+
+unix:!macx:LIBS += -L./lib -lchartdir
+unix:!macx:QMAKE_RPATHDIR += ./lib
